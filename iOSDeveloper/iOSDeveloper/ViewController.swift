7c075b1443591ea3480f45f744d10a63207d4205
chettalek <110464221+chettalek@users.noreply.github.com>
2024-06-05 23:09:18 +0700

    Add files via upload

---
+++ iOSDeveloper/iOSDeveloper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSDeveloper
+//
+//  Created by Diamond Pi on 5/6/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
